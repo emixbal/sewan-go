920255503de5d78023233ebeea392fb553f88b09
iqbal <emixbal@gmail.com>
2022-03-16 13:24:50 +0700

    init transaction

---
+++ app/models/transaction.models.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Transaction struct {
+	ID         uint `json:"id"`
+	IsActive   bool `json:"is_active,omitempty" gorm:"default:true"`
+	CustomerID int
+	Customer   Customer
+	StartDate  time.Time `gorm:"not null" json:"start_date"`
+	EndDate    time.Time `gorm:"not null" json:"end_date"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}

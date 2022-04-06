6483d6e9c4cac96e26e2b91359c4099c5484d8d4
iqbal <emixbal@gmail.com>
2022-04-06 14:53:20 +0700

    change payload to json

--- app/controllers/transactionCustomerNew.controllers.go
+++ app/controllers/transactionCustomerNew.controllers.go
@@ -14,37 +14,54 @@ func TransactionCustomerNew(c *fiber.Ctx) error {
 	var customer models.Customer
 	var transaction models.Transaction
 
-	customer.Name = c.FormValue("name")
-	customer.Address = c.FormValue("address")
-	customer.Phone = c.FormValue("phone")
-	customer.Email = c.FormValue("email")
+	payload := struct {
+		Name      string `json:"name"`
+		Address   string `json:"address"`
+		Phone     string `json:"phone"`
+		Email     string `json:"email"`
+		StartDate string `json:"start_date"`
+		EndDate   string `json:"end_date"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return err
+	}
+
+	customer.Name = payload.Name
+	customer.Address = payload.Address
+	customer.Phone = payload.Phone
+	customer.Email = payload.Email
 
 	if customer.Name == "" {
+		log.Println("name is required")
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "name is required"})
 	}
 
 	if customer.Address == "" {
+		log.Println("address is required")
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "address is required"})
 	}
 
 	if customer.Phone == "" {
+		log.Println("phone is required")
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "phone is required"})
 	}
 
 	layout := "2006-01-02"
-	start_date, err_start := time.Parse(layout, c.FormValue("start_date"))
+	start_date, err_start := time.Parse(layout, payload.StartDate)
 	if err_start != nil {
 		log.Println("err_start==> ", err_start)
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "start_date is empty or invalid format"})
 	}
 
-	end_date, err_end := time.Parse(layout, c.FormValue("end_date"))
+	end_date, err_end := time.Parse(layout, payload.EndDate)
 	if err_end != nil {
 		log.Println("err_end==> ", err_end)
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "end_date is empty or invalid format"})
 	}
 
 	if start_date.Before(end_date) == false {
+		log.Println("end_date must be after start_date")
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "end_date must be after start_date"})
 	}
 

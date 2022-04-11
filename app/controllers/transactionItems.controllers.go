b96f7ae43e675d806a40c8dd51340f151068e165
iqbal <emixbal@gmail.com>
2022-04-11 15:39:51 +0700

    change format

--- app/controllers/transactionItems.controllers.go
+++ app/controllers/transactionItems.controllers.go
@@ -18,21 +18,26 @@ func AddItemToTransaction(c *fiber.Ctx) error {
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "transaction_id is empty or invalid format"})
 	}
 
-	product_id, err_product_id := strconv.Atoi(c.FormValue("product_id"))
-	if err_product_id != nil {
-		log.Println("err_product_id==> ", err_product_id)
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "product_id is empty or invalid format"})
+	payload := struct {
+		ProductId int `json:"product_id"`
+		Qty       int `json:"qty"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return err
 	}
 
-	qty, err_qty := strconv.Atoi(c.FormValue("qty"))
-	if err_qty != nil {
-		log.Println("err_qty==> ", err_qty)
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "qty is empty or invalid format"})
+	if payload.ProductId == 0 {
+		return c.Status(http.StatusBadRequest).JSON(map[string]string{"message": "product_id is required"})
+	}
+
+	if payload.Qty == 0 {
+		return c.Status(http.StatusBadRequest).JSON(map[string]string{"message": "qty is required"})
 	}
 
 	item.TransactionID = transaction_id
-	item.ProductID = product_id
-	item.Qty = qty
+	item.ProductID = payload.ProductId
+	item.Qty = payload.Qty
 
 	result, _ := models.AddItemToTransaction(&item)
 	return c.Status(result.Status).JSON(result)

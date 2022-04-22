fdd3c9b5c84d35223d2cc7b0bef35b147c6152bd
iqbal <emixbal@gmail.com>
2022-04-22 10:43:56 +0700

    disable postgres

--- config/gorm.config.go
+++ config/gorm.config.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -25,16 +24,16 @@ func InitDB() *gorm.DB {
 		return DB
 	}
 
-	if os.Getenv("DB_DRIVER") == "postgres" {
-		// DSN := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-		DSN := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=Asia/Jakarta"
-		DB, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	// if os.Getenv("DB_DRIVER") == "postgres" {
+	// 	// DSN := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	// 	DSN := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=Asia/Jakarta"
+	// 	DB, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
 
-		if err != nil {
-			panic("connectionString error")
-		}
-		return DB
-	}
+	// 	if err != nil {
+	// 		panic("connectionString error")
+	// 	}
+	// 	return DB
+	// }
 	fmt.Println("DB_DRIVER not supported")
 	return nil
 }
